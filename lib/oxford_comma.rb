9bcd3f2ee1eea020e3958a9ea410602fcf7e3f42
Plimon Howard V <plimon.howard@gmail.com>
2020-09-08 21:21:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if (array.size == 1 )
+    return array.join(", ")
+  elsif (array.size == 2)
+    return array.join(" and ")
+  elsif (array.size == 3)
+    array[-1] = "and " + array[-1]
+    return array.join(", ")
+  else
+    array[-1] = "and " + array[-1]
+    return array.join(", ")
+  end
 end
