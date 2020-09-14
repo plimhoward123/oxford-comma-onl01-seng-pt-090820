4695d8bfe7019857eb44d3e9b20df3b3c7bd52e7
Plimon Howard V <plimon.howard@gmail.com>
2020-09-14 21:51:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if (array.size == 1 )
-    return array.join(", ")
+    return array.join(" , ")
   elsif (array.size == 2)
     return array.join(" and ")
   elsif (array.size == 3)
